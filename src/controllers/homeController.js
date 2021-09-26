17ff2008260636783ce2e2046a775b52c3103b97
Beatris Borislavova <betivd03@abv.bg>
2021-09-26 14:12:01 +0300

    Add about page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,8 +6,13 @@ const router = express.Router();
 const home = (req, res) => {
     let cubes = cubeService.getAll();
     res.render('index', { cubes });
+};
+
+const about = (req, res) => {
+    res.render('about');
 }
 
 router.get('/', home);
+router.get('/about', about);
 
 module.exports = router;
