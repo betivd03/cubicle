858ff4dc8cbe5c172b246043b250b39cbb07fed8
Beatris Borislavova <betivd03@abv.bg>
2021-10-01 18:41:48 +0300

    Add mongoose library and cube model

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const routes = require('./routes.js');
 const config = require('./config/config.json')[process.env.NODE_ENV]; // [process.env.NODE_ENV || 'development']
+const initDataBase = require('./config/database.js');
 
 // const initHandlebars = require('./config/handlebars.js');
 
@@ -17,4 +18,10 @@ require('./config/handlebars.js')(app);
 app.use(express.static(path.resolve(__dirname, './public')));
 app.use(routes);
 
-app.listen(config.PORT, console.log.bind(console, `Application is running on http://localhost:${config.PORT}`));
+initDataBase(config.DB_CONNECTION_STRING)
+    .then(() => {
+        app.listen(config.PORT, console.log.bind(console, `Application is running on http://localhost:${config.PORT}`));
+    })
+    .catch((err) => {
+        console.log('Application init failed: ', err);
+    })
--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,46 +1,86 @@
-const uniqid = require('uniqid');
+const mongoose = require('mongoose');
 
-class Cube {
-    static #cubes = [
-        {
-            id: 'asdfghjklkjhgfdsa',
-            name: 'Mirror Cube',
-            description: 'Cool Rubik Cube :D',
-            imageUrl: 'https://m.media-amazon.com/images/I/41KNQRXAYvL._AC_SY580_.jpg',
-            difficulty: '4'
-        },
-        {
-            id: '9m0r7wkku13twxj',
-            name: 'Megaminx',
-            description: 'Different Rubik Cube',
-            imageUrl: 'http://cdn.shopify.com/s/files/1/0035/5205/1273/products/DaYan_20Megaminx_20V2_20M_1200x1200.jpg?v=1607362354',
-            difficulty: '5'
-        },
-        {
-            id: '9m0r3qsku1l6eye',
-            name: "Rubik's Cube 5x5x5",
-            description: "Difficult Rubik's Cube :D",
-            imageUrl: 'https://i5.walmartimages.com/asr/76eb79aa-1c55-4b0b-90d5-7d9a408a9e00_1.9b982635b711b3e13d3b8e8d18daf083.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff',       
-            difficulty: '6'
+const cubeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 100,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        // validate: /^https?:\/\//i
+        validate: {
+            validator: function(value) {
+                return /^https?:\/\//i.test(value);
+            },
+            message: 'Image Url is invalid!'
         }
-    ];
-
-    constructor(name, description, imageUrl, difficulty) {
-        this.id = uniqid();
-        this.name = name;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this.difficulty = difficulty;
-        // this.canonicalUrl = name.replace(/ /g, '-').toLowerCase();
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 6,
     }
+});
 
-    static get cubes() {
-        return Cube.#cubes.slice();
-    }
+// cubeSchema.path('imageUrl').validate = function(value) {
+//     return /^https?:\/\//i.test(value);
+// }
 
-    static add(cube) {
-        Cube.#cubes.push(cube);
-    }
-}
-
+const Cube = mongoose.model('Cube', cubeSchema);
 module.exports = Cube;
+
+
+
+// const uniqid = require('uniqid');
+
+// class Cube {
+//     static #cubes = [
+//         {
+//             id: 'asdfghjklkjhgfdsa',
+//             name: 'Mirror Cube',
+//             description: 'Cool Rubik Cube :D',
+//             imageUrl: 'https://m.media-amazon.com/images/I/41KNQRXAYvL._AC_SY580_.jpg',
+//             difficulty: '4'
+//         },
+//         {
+//             id: '9m0r7wkku13twxj',
+//             name: 'Megaminx',
+//             description: 'Different Rubik Cube',
+//             imageUrl: 'http://cdn.shopify.com/s/files/1/0035/5205/1273/products/DaYan_20Megaminx_20V2_20M_1200x1200.jpg?v=1607362354',
+//             difficulty: '5'
+//         },
+//         {
+//             id: '9m0r3qsku1l6eye',
+//             name: "Rubik's Cube 5x5x5",
+//             description: "Difficult Rubik's Cube :D",
+//             imageUrl: 'https://i5.walmartimages.com/asr/76eb79aa-1c55-4b0b-90d5-7d9a408a9e00_1.9b982635b711b3e13d3b8e8d18daf083.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff',       
+//             difficulty: '6'
+//         }
+//     ];
+
+//     constructor(name, description, imageUrl, difficulty) {
+//         this.id = uniqid();
+//         this.name = name;
+//         this.description = description;
+//         this.imageUrl = imageUrl;
+//         this.difficulty = difficulty;
+//         // this.canonicalUrl = name.replace(/ /g, '-').toLowerCase();
+//     }
+
+//     static get cubes() {
+//         return Cube.#cubes.slice();
+//     }
+
+//     static add(cube) {
+//         Cube.#cubes.push(cube);
+//     }
+// }
+
+// module.exports = Cube;
