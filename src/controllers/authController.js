9eba787cf57bb09602d9eec28761d3055c34105c
Beatris Borislavova <betivd03@abv.bg>
2021-10-13 18:33:07 +0300

    Add login post action

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
+});
+
+router.post('/login', (req, res) => {
+    console.log(req.body);
+
+    res.redirect('/login');
 })
 
 module.exports = router;
