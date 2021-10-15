28536531c12672dd7207b9f320643ea8348f4662
Beatris Borislavova <betivd03@abv.bg>
2021-10-15 20:56:32 +0300

    Redirect to 404 if unsuccessful login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,9 +9,16 @@ router.get('/login', (req, res) => {
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
     
-    let auth = await authService.login(username, password);
+    let user = await authService.login(username, password);
 
-    res.redirect('/login');
+    console.log(user);
+
+    if (user) {
+        res.redirect('/');
+    } else {
+        res.redirect('/404');
+    }
+
 });
 
 router.get('/register', (req, res) => {
--- src/services/authService.js
+++ src/services/authService.js
@@ -11,7 +11,8 @@ exports.register = function(username, password, repeatPassword) {
 
 exports.login = function(username, password, repeatPassword) {
     return User.findByUsername(username)
-        .then(user => Promise.all[bcrypt.compare(password, user.password), user]})
+        // .then(user => Promise.all[bcrypt.compare(password, user.password), user])
+        .then(user => Promise.all([user.validatePassword(password), user]))
         .then(([isValid, user]) => {
             if (isValid) {
                 return user;
@@ -19,4 +20,5 @@ exports.login = function(username, password, repeatPassword) {
                 throw { message: 'Cannot find username or password'}
             }
         })
+        .catch(() => null);
 }
